dab69e74821bb391a7b34326b83347a9a889cda3
Dino <dnp.digital@gmail.com>
2024-12-17 00:07:09 +0200

    123

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,4 @@
 import { Plugin } from 'vite';
-import * as Van from 'vanjs-core';
 
 declare function vanjsPlugin(): Plugin;
 export default vanjsPlugin;
-
-export { Van };
--- vanjs-ssr.d.ts
+++ vanjs-ssr.d.ts
@@ -2,7 +2,13 @@
 
 declare module 'vanjs/ssr' {
     // export default Van;
-    import Van from 'vanjs-core';
-    export default Van;
-    
+    import van from 'vanjs-core';
+    export default van;
 }
+
+declare module 'vanjs' {
+    import alias from 'vanjs/ssr';
+    export default alias;
+}
+
+// export declare const vanjs: typeof import('vanjs/ssr');
