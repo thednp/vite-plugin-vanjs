e4db1c961357684f12bdc2db887ee0553a8769a9
Dino <dnp.digital@gmail.com>
2024-12-16 22:33:17 +0200

    fix?

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,7 @@
-import { Plugin } from 'vite';
+import van from 'vanjs-core';
+import vanPlate from 'mini-van-plate/van-plate';
 
-declare function vanjsPlugin(): Plugin;
-export default vanjsPlugin;
+export { vanCore as van, vanPlate };
+
+declare const _default: typeof vanPlate | typeof van;
+export default _default;
