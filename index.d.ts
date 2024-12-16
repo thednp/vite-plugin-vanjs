6eb84886ad35fd06f9d0934849d39564737653c2
Dino <dnp.digital@gmail.com>
2024-12-16 17:24:46 +0200

    cleanup

--- index.d.ts
+++ index.d.ts
@@ -1,20 +1,4 @@
 import { type Plugin } from 'vite';
-// import { type Van } from "vanjs-core";
-import van from "vanjs-core";
-
-// declare module "vanjs/ssr" {
-//     export default Van;
-// }
-
-declare module 'vanjs/ssr' {
-    export default van;
-
-    // Add any necessary declarations for the vanjs/ssr module here
-}
-
-declare global {
-    export const van = "vanjs/ssr"; // Or specify the correct type if known
-}
 
 declare function vanjsPlugin(): Plugin;
 export default vanjsPlugin;
