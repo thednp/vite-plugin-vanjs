8d6ac6fdc1aaf7e126c1db41fca99d4999e4b276
Dino <dnp.digital@gmail.com>
2024-12-22 11:26:34 +0200

    more

--- setup/global.d.ts
+++ setup/global.d.ts
@@ -1,13 +1,24 @@
-import setup from "./index.mjs";
+// import setup from "./index.mjs";
 
-declare module "@vanjs/setup" {
-    export default setup;
+import { dummyVanX } from "mini-van-plate/shared";
+import vanPlate from "mini-van-plate/van-plate";
+import vanCore from "vanjs-core";
+import * as vanX from "vanjs-ext";
+
+type VansSetup = {
+    isServer: boolean,
+    van: typeof vanPlate & typeof vanCore,
+    vanX: typeof dummyVanX | typeof vanX,
 }
-declare module "@vanjs/van" {
+
+export declare module "@vanjs/setup" {
+    export default VansSetup;
+}
+export declare module "@vanjs/van" {
     import v from "vite-plugin-vanjs/setup";
     export default v.van;
 }
-declare module "@vanjs/vanX" {
+export declare module "@vanjs/vanX" {
     import v from "vite-plugin-vanjs/setup";
     export default v.vanX;
 }
