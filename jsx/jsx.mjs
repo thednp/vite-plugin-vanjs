24bd288515571125c37800eca13d069c9208c350
Dino <dnp.digital@gmail.com>
2024-12-24 20:23:12 +0200

    fix SSR

--- jsx/jsx.mjs
+++ jsx/jsx.mjs
@@ -1,38 +1,43 @@
 import van from "@vanjs/van";
+import setup from "@vanjs/van";
 import { setAttribute, styleToString } from "./utils.mjs";
 
 export const jsx = (jsxTag, { children, ref, style, ...props }) => {
   if (typeof jsxTag === "string") {
-    const ele = van.tags[jsxTag](children);
+    const newElement = van.tags[jsxTag](props, children);
 
+    // on server it's good enough to return here
+    if (setup.isServer) return newElement;
+
+    // on the client, we sure do need to set the attributes
     for (const [k, value] of Object.entries(props)) {
       if (typeof value === "function" && !k.startsWith("on")) {
-        van.derive(() => setAttribute(ele, k, value()));
+        van.derive(() => setAttribute(newElement, k, value()));
         continue;
       }
 
       if (typeof value === "function" && k.startsWith("on")) {
-        ele.addEventListener(k.slice(2).toLowerCase(), value);
+        newElement.addEventListener(k.slice(2).toLowerCase(), value);
         continue;
       }
 
       if (typeof value === "object" && "val" in value) {
-        van.derive(() => setAttribute(ele, k, value.val));
+        van.derive(() => setAttribute(newElement, k, value.val));
         continue;
       }
 
-      setAttribute(ele, k, value);
+      setAttribute(newElement, k, value);
     }
     if (style) {
       if (typeof style === "function") {
-        van.derive(() => ele.style.cssText = styleToString(style()));
+        van.derive(() => newElement.style.cssText = styleToString(style()));
       } else {
-        ele.style.cssText = styleToString(style);
+        newElement.style.cssText = styleToString(style);
       }
     }
 
-    if (ref) ref.val = ele;
-    return ele;
+    if (ref) ref.val = newElement;
+    return newElement;
   }
 
   return typeof jsxTag === "function"
