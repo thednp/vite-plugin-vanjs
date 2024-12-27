175dc4303c7c7635a2d9764a1a3a60c104bb6d6c
Dino <dnp.digital@gmail.com>
2024-12-27 14:58:32 +0200

    fix types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,4 +1,7 @@
 import type { Plugin } from "vite";
 export * from "../jsx/types";
 export * from "../setup/types";
+export * from "../router/types";
+export * from "../server/types";
+export * from "../dom/types";
 export default function VitePluginVanJS(): Plugin;
